2af3f2a86be252f6e008985e1008eebdecd4ee7e
Slangeland1 <stephanielangeland@gmail.com>
2017-02-11 19:41:38 -0500

    updates
    updates

---
+++ Final Project_v1.R
@@ -0,0 +1,18 @@
+# #######################################################################
+# File-Name:  Final Project_v1
+# Version:  1
+# Date:  02/11/17
+# Author:  Stephanie Langeland
+# Purpose:  Explore data
+# Input Files: ? 
+# Output Files: ?
+# Data Output: ?
+# Previous files: None
+# Dependencies: ?
+# Required by: ?
+# Status: In progress
+# Machine:  Stephanie's 2011 MacBook Pro
+# #######################################################################
+
+setwd("/Users/StephanieLangeland/Desktop/Columbia/Applied Data Science/Git/QMSS_G5069_Applied_D_S/data/raw")
+load("")
