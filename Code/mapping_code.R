850dc2f97ea288d53d259f413003e77e4e49be74
amp5 <amp5@users.noreply.github.com>
2017-03-01 18:32:29 -0500

    updated mapping code

--- Code/mapping_code.R
+++ Code/mapping_code.R
@@ -5,7 +5,7 @@
 # Author:  amp5
 # Purpose:  massage data into format for mapping in QGIS
 # Input Files:  2015_stopandfrisk_CLEAN.csv
-# Output Files: 2015_stopandfrisk_CLEAN_w_counties.csv , 2015_SAF_county.csv, bronx.csv
+# Output Files: 2015_stopandfrisk_CLEAN_w_counties.csv , 2015_SAF_county.csv, bronx.csv, bronx_agg_pf.csv
 # Data Output: ?
 # Previous files: Final Project_v1.R
 # Dependencies: ?
@@ -18,11 +18,15 @@
 library(tidyverse)
 
 df <- read.csv('data/2015_stopandfrisk_CLEAN.csv')
+
 # orig df had 22563 rows, after all rows w/ NA in xcoord removed, 
 # df has 21747
 nrow(df)
 
 df <- filter(df, !is.na(xcoord))
+
+# orig df had 22563 rows, after all rows w/ NA in xcoord removed, 
+# df has 21747
 
 df$county <- ifelse(df$city == 'Manhattan', 'New York', 
                     ifelse(df$city == "Bronx", 'Bronx', 
@@ -41,14 +45,18 @@ df$geoid <- ifelse(df$city == 'Manhattan', 36061,
 
 write.csv(df, '2015_stopandfrisk_CLEAN_w_counties.csv')
 
+data_all <- read_csv("/Users/alexandraplassaras/Desktop/Spring_2017/QMSS_G5069/QMSS_G5069_Applied_D_S-master/data/2015_stopandfrisk_CLEAN_w_counties.csv")
+
 
 ## For now only looking at coords, county and pforce
 
-map_df <- df[,c('xcoord', 'ycoord', 'county', 'geoid', 'pforce')]
+map_df <- data_all[,c('xcoord', 'ycoord', 'county', 'geoid', 'pforce', 'race', 
+                      'rf_furt', 'rf_bulg', 'rf_knowl', 'rf_verbl', 'rf_rfcmp',
+                      'rf_vcact', 'rf_attir', 'rf_othsw', 'rf_vcrim')]
 
 
 ### need to sum pforce for each county
-map_sum <- aggregate(pforce ~ county, data=df, FUN=sum)
+map_sum <- aggregate(pforce ~ county, data=data_all, FUN=sum)
 
 map_sum$geoid <- c(36005, 36047, 36061, 36081, 36085)
 
@@ -59,24 +67,41 @@ write.csv(map_sum, '2015_SAF_county.csv')
 ## now look at bronx:
 
   
-bronx <- filter(df, county %in% c('Bronx'))
+bronx <- filter(data_all, county %in% c('Bronx'))
 write.csv(bronx, 'bronx.csv')
 
 
 # summing pforce for each unique location
-bronx_p <- bronx[,c('xcoord', 'ycoord', 'county', 'geoid', 'pforce')]
+bronx_p <- bronx[,c('xcoord', 'ycoord', 'county', 'geoid', 'pforce', 'race', 
+                    'rf_furt', 'rf_bulg', 'rf_knowl', 'rf_verbl', 'rf_rfcmp',
+                    'rf_vcact', 'rf_attir', 'rf_othsw', 'rf_vcrim')]
 # some reason lost 66 pforce in next var.... there are NA xcoord and ycoord in above var
 # aggregate doesn't count this
 # TODO: go back and remove any NA coords rows (~172ish)
 bronx_unq_p <- aggregate(pforce ~ xcoord + ycoord, data = bronx_p, FUN = sum)
 
-nrow(bronx_unq_p)
-# 2220 unique locations for SaF
 
-bronx_pf_unq <- filter(bronx_unq_p, pforce > 0)
-nrow(bronx_pf_unq)
+bronx_agg <- aggregate(cbind(pforce, rf_furt, rf_bulg, rf_knowl, rf_verbl, rf_rfcmp,
+                    rf_vcact, rf_attir, rf_othsw, rf_vcrim) ~ xcoord + ycoord, data = bronx_p, FUN = sum)
+
+bronx_agg %>%
+  nrow()
+
+
+bronx_agg_pf <-  bronx_agg[bronx_agg$pforce > 0,]
+
+
+
+# 2220 unique locations for SaF
 # 1199 unique locatiosn with some police force
 
-(sum(bronx_pf_unq$pforce))
 
-write.csv(bronx_pf_unq, 'bronx_pf_unq.csv')
+write.csv(bronx_agg_pf, 'bronx_agg_pf.csv')
+
+
+### not able to find way to transform coords into block areas.
+# will attempt to do so using addresses....
+
+
+
+
