95daa499887926fa8c22729a73901062e26ce3fb
Slangeland1 <stephanielangeland@gmail.com>
2017-04-25 16:48:46 -0400

    Zach's code is done

--- Code/DataScienceBMLMSkeleton.R
+++ Code/DataScienceBMLMSkeleton.R
@@ -9,11 +9,13 @@
 # Data Output: none  
 # Previous files: None
 # Dependencies: None
-# Required by: Data Analysis Final Project
+# Required by: Data Science Final Project
 # Status: In progress 
 # Machine: MacBook 
 # R version 3.3.3
 #############################################################
+
+
 
 ##::::::::::::::::: Set path:
 
@@ -21,7 +23,9 @@
 
 stop_frisk <- read.csv("/Users/StephanieLangeland/Desktop/Columbia/Applied Data Science/Git/QMSS_G5069_Applied_D_S/Data+Code Book/Cleaned/2015_stopandfrisk_CLEAN_w_counties.csv") ## stephanie's path
 
-##::::::::::::::::: Prepare the data:
+
+
+##::::::::::::::::: Clean/prepare the date data:
 stop_frisk$datestop <- as.character(stop_frisk$datestop) 
 
 for(i in 1:21747){
@@ -58,6 +62,9 @@ for(i in 1:21747){
 
 stop_frisk$datestop <- paste0("2015", stop_frisk$datestop)
 
+
+
+##::::::::::::::::: Subset Bronx data:
 sf_bronx <- stop_frisk[stop_frisk$city == 'Bronx', ]
 
 sf_bronx$race <- as.factor(sf_bronx$race)
@@ -70,7 +77,14 @@ sf_bronx$time <- as.numeric(sf_bronx$datestop)
 sf_bronx$time <- sf_bronx$time - 16435
 
 library(rethinking)
-sf_bronx <- sf_bronx[is.na(sf_bronx$race) == FALSE & is.na(sf_bronx$sex) == FALSE & is.na(sf_bronx$build) == FALSE & is.na(sf_bronx$time) == FALSE, ]
+sf_bronx <- sf_bronx[is.na(sf_bronx$race) == FALSE & 
+                       is.na(sf_bronx$sex) == FALSE & 
+                       is.na(sf_bronx$build) == FALSE & 
+                       is.na(sf_bronx$time) == FALSE, ]
+
+
+
+##::::::::::::::::: Create Bronx dataset:
 mdata <- list(Force = sf_bronx$pforce, 
               AIAN = (sf_bronx$race == "American Indian / Alaskan Native"), 
               AAPI = (sf_bronx$race == "Asian / Pacific Islander"), 
@@ -83,32 +97,60 @@ mdata <- list(Force = sf_bronx$pforce,
               Thin = (sf_bronx$build == "Thin"),
               Time = sf_bronx$time)
 
+## Model will include the following variables:  
 model.geo <- alist(
   Force ~ dbinom(1, p),
-  logit(p) <- a_sub[Precinct] + b_aian[Precinct]* AIAN + b_aapi[Precinct] * AAPI + b_black[Precinct] * Black + b_hisp[Precinct] * Hispanic + b_gender[Precinct] * Female + b_medium[Precinct] * Medium + b_muscular[Precinct] * Muscular + b_thin[Precinct] * Thin + b_time[Precinct] * Time,
-  ## we would need a b_sub term for every predictor
-  
-  c(a_sub,b_aian,b_aapi,b_black,b_hisp, b_gender, b_medium, b_muscular, b_thin, b_time)[Precinct] ~ dmvnorm2(c(a,baian,baapi,bblack,bhispanic,bgender,bmedium,bmuscular,bthin,btime),sigma_sub,Rho),
-  ## this needs to include a term for each of the model predictors
-  
-  a ~ dnorm(0,2),
-  baian ~ dnorm(0,3),
-  baapi ~ dnorm(0,3),
-  bblack ~ dnorm(0,3),
-  bhispanic ~ dnorm(0,3),
-  bgender ~ dnorm(0,3),
-  bmedium ~ dnorm(0,3),
-  bmuscular ~ dnorm(0,3),
-  bthin ~ dnorm(0,3),
-  btime ~ dnorm(0,3),
-  
-  ##needs to be one of these specifications for each of the terms; we can play with priors
-  
-  sigma_sub ~ dcauchy(0,2),
+  logit(p) <- a_sub[Precinct] + 
+    b_aian[Precinct]* AIAN + 
+    b_aapi[Precinct] * AAPI + 
+    b_black[Precinct] * Black + 
+    b_hisp[Precinct] * Hispanic + 
+    b_gender[Precinct] * Female + 
+    b_medium[Precinct] * Medium + 
+    b_muscular[Precinct] * Muscular + 
+    b_thin[Precinct] * Thin + 
+    b_time[Precinct] * Time, ## we would need a b_sub term for every predictor
+  c(a_sub,
+    b_aian,
+    b_aapi,
+    b_black,
+    b_hisp, 
+    b_gender, 
+    b_medium, 
+    b_muscular, 
+    b_thin, 
+    b_time)[
+      Precinct] ~ 
+    dmvnorm2(c(a,
+               baian,
+               baapi,
+               bblack,
+               bhispanic,
+               bgender,
+               bmedium,
+               bmuscular,
+               bthin,
+               btime),
+             sigma_sub,
+             Rho), ## this needs to include a term for each of the model predictors
+  a ~ dnorm(0, 2),
+  baian ~ dnorm(0, 3),
+  baapi ~ dnorm(0, 3),
+  bblack ~ dnorm(0, 3),
+  bhispanic ~ dnorm(0, 3),
+  bgender ~ dnorm(0, 3),
+  bmedium ~ dnorm(0, 3),
+  bmuscular ~ dnorm(0, 3),
+  bthin ~ dnorm(0, 3),
+  btime ~ dnorm(0, 3), ##needs to be one of these specifications for each of the terms; we can play with priors
+  sigma_sub ~ dcauchy(0, 2),
   Rho ~ dlkjcorr(4)
 )
 
+## Final model:
 run.model <- map2stan(model.geo,
-                      data=mdata, iter=7000, warmup=2000, chains=4,
-                      cores=4)
+                      data = mdata, iter = 7000, warmup = 2000, chains = 4,
+                      cores = 4)
+
 precis(run.model, depth = 2, digits = 5)
+
--- Code/Marascuilo Procedure.R
+++ Code/Marascuilo Procedure.R
@@ -1,22 +1,51 @@
-population <- c(7980,53123, 475378, 299869, 146928)
-stops <- c(76,1043, 12919, 2389, 4907)
+#############################################################
+# File-Name:  Marascuilo Procedure.R
+# Version: 1
+# Date: 04/25/17
+# Author: Zachary Heinemann 
+# Purpose: Marascuilo Procedure
+# Input Files: None
+# Output Files: None yet
+# Data Output: none  
+# Previous files: None
+# Dependencies: None
+# Required by: Data Science Final Project
+# Status: In progress 
+# Machine: MacBook 
+# R version 3.3.3
+#############################################################
+
+
+population <- c(7980, 53123, 475378, 299869, 146928) ## Bronx population of each race
+stops <- c(76, 1043, 12919, 2389, 4907) ## Numver of stop and frisk events by each race noted above
 
 p <- stops / population
-
 
 N <- length(p)
 value <- critical.range <- c()
 
-## Compute critical values.
+## Compute critical values.:
 for (i in 1:(N-1))
 { for (j in (i+1):N)
 {
-  value = c(value,(abs(p[i]-p[j])))
+  value = c(value, (abs(p[i]-p[j])))
   critical.range = c(critical.range,
-                     sqrt(qchisq(.95,3))*sqrt(p[i]*(1-p[i])/population[i] + p[j]*(1-p[j])/population[i]))
+                     sqrt(qchisq(0.95, 3)) * sqrt(p[i] * (1 - p[i]) / population[i] + 
+                                                    p[j] * (1 - p[j]) / population[i]))
 }
 }
-differences <- round(cbind(value,critical.range),3)
-rownames(differences) <- c("AIAN - AAPI", "AIAN - Black", "AIAN - White", "AIAN - Hisp", "AAPI - Black", "AAPI - White", "AAPI - Hisp", "Black - White", "Black - Hisp", "White - Hisp")
+
+differences <- round(cbind(value, critical.range), 3)
+rownames(differences) <- c("AIAN - AAPI", 
+                           "AIAN - Black", 
+                           "AIAN - White", 
+                           "AIAN - Hisp", 
+                           "AAPI - Black", 
+                           "AAPI - White", 
+                           "AAPI - Hisp", 
+                           "Black - White", 
+                           "Black - Hisp", 
+                           "White - Hisp")
+
 differences <- as.data.frame(differences)
 differences$sig <- ifelse(differences$value > differences$critical.range, "*", NA)
